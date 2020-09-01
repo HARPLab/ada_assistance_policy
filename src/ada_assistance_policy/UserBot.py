0c06fe361806627bfa8333a125625112b0d3ba85
Reuben Aronson <rmaronson@cmu.edu>
2020-09-01 19:47:37 -0400

    refactoring bc i'm picky

--- src/ada_assistance_policy/UserBot.py
+++ src/ada_assistance_policy/UserBot.py
@@ -1,11 +1,13 @@
 import numpy as np
 from Goal import *
 #from Robot import Robot
+from input_handlers.UserInputListener import UserInputData
+import AssistancePolicyOneGoal
 
 class UserBot:
-    def __init__(self, goals):
+    def __init__(self, goals, robot):
         self.goals = goals
-        self.goal_num = 0;
+        self.goal_num = 0
 
         # scaling factors for generating commands
         self.position_scale_vector = 10.
@@ -14,12 +16,31 @@ class UserBot:
         self.usr_cmd_dim = 3
         self.reset_noise_filter()
 
+        # Use the goal policies to figure out our actual target
+        self.goal_policies = [ AssistancePolicyOneGoal.AssistancePolicyOneGoal(g) for g in goals ]
+        self.robot = robot
+
     def set_user_goal(self, goal_num):
 #        num_goals = self.robot.world.num_goals();
 #        if (goal_num >= num_goals):
 #            raise Exception('Desired goal', goal_num, 
 #                    'exceeds max goal number', num_goals-1)
         self.goal_num = goal_num  
+
+    def get_user_command(self):
+        # update the underlying policy with our current position
+        # note that we don't care about the applied user action
+        # since we only need V not Q
+        # to pick the best target
+        ee_trans = self.robot.arm.getEndEffectorTransform()
+        self.goal_policies[self.goal_num].update(ee_trans, np.zeros((6, 1)))
+        goal_pose = self.goal_policies[self.goal_num].get_min_value_pose()
+        cmd = UserInputData(self.get_usr_cmd(ee_trans, goal_pose))
+        if self.goals[self.goal_num].at_goal(ee_trans):
+            cmd.close_hand_velocity = 1.0 # Pretty sure this is incorrect but it's how it was in the original.....
+        else:
+            cmd.close_hand_velocity = 0.
+        return cmd
 
     def get_usr_cmd(self, end_effector_trans, goal_pose=None):
         if goal_pose is None:
@@ -32,7 +53,7 @@ class UserBot:
           pos_diff /= pos_diff_norm/self.clip_norm_val
 
         # add colored noise to the command
-        usr_cmnd = pos_diff;
+        usr_cmnd = pos_diff
         usr_cmnd[0:2] *= -1.
         #usr_cmnd += self.noise_pwr*np.linalg.norm(usr_cmnd)*np.random.randn(self.usr_cmd_dim)  
         #usr_cmnd += self.correl_coeff.dot(self.white_noise_hist)
